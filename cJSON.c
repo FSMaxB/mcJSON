343ed66ac62142f8bf8cc74471ed8aa83464c75f
Max Bruckner <max@maxbruckner.de>
2015-10-17 11:12:48 +0200

    codestyle: parse_object

--- cJSON.c
+++ cJSON.c
@@ -862,39 +862,68 @@ static char *print_array(cJSON *item, int depth, int fmt, printbuffer *p) {
 }
 
 /* Build an object from the text. */
-static const char *parse_object(cJSON *item,const char *value)
-{
+static const char *parse_object(cJSON *item, const char *value) {
 	cJSON *child;
-	if (*value!='{')	{ep=value;return 0;}	/* not an object! */
-
-	item->type=cJSON_Object;
-	value=skip(value+1);
-	if (*value=='}') return value+1;	/* empty array. */
-
-	item->child=child=cJSON_New_Item();
-	if (!item->child) return 0;
-	value=skip(parse_string(child,skip(value)));
-	if (!value) return 0;
-	child->string=child->valuestring;child->valuestring=0;
-	if (*value!=':') {ep=value;return 0;}	/* fail! */
-	value=skip(parse_value(child,skip(value+1)));	/* skip any spacing, get the value. */
-	if (!value) return 0;
-
-	while (*value==',')
-	{
-		cJSON *new_item;
-		if (!(new_item=cJSON_New_Item()))	return 0; /* memory fail */
-		child->next=new_item;new_item->prev=child;child=new_item;
-		value=skip(parse_string(child,skip(value+1)));
-		if (!value) return 0;
-		child->string=child->valuestring;child->valuestring=0;
-		if (*value!=':') {ep=value;return 0;}	/* fail! */
-		value=skip(parse_value(child,skip(value+1)));	/* skip any spacing, get the value. */
-		if (!value) return 0;
+	if (*value != '{') { /* not an object! */
+		ep = value;
+		return 0;
 	}
 
-	if (*value=='}') return value+1;	/* end of array */
-	ep=value;return 0;	/* malformed. */
+	item->type = cJSON_Object;
+	value = skip(value + 1);
+	if (*value=='}') { /* empty object. */
+		return value + 1;
+	}
+
+	child = cJSON_New_Item();
+	item->child = child;
+	if (!item->child) {
+		return 0;
+	}
+	value = skip(parse_string(child, skip(value)));
+	if (!value) {
+		return 0;
+	}
+	child->string = child->valuestring;
+	child->valuestring = 0;
+	if (*value != ':') { /* fail! */
+		ep = value;
+		return 0;
+	}
+	value = skip(parse_value(child, skip(value + 1))); /* skip any spacing, get the value. */
+	if (!value) {
+		return 0;
+	}
+
+	while (*value == ',') {
+		cJSON *new_item;
+		if (!(new_item = cJSON_New_Item())) { /* memory fail */
+			return 0;
+		}
+		child->next = new_item;
+		new_item->prev = child;
+		child = new_item;
+		value = skip(parse_string(child, skip(value + 1)));
+		if (!value) {
+			return 0;
+		}
+		child->string = child->valuestring;
+		child->valuestring = 0;
+		if (*value != ':') { /* fail! */
+			ep = value;
+			return 0;
+		}
+		value = skip(parse_value(child, skip(value + 1))); /* skip any spacing, get the value. */
+		if (!value) {
+			return 0;
+		}
+	}
+
+	if (*value == '}') { /* end of object */
+		return value + 1;
+	}
+	ep = value;
+	return 0; /* malformed. */
 }
 
 /* Render an object to text. */
