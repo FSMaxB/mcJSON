a8eaf441e15f81782e2f2f7d36013c2b890bb985
Max Bruckner <max@maxbruckner.de>
2015-10-17 03:48:09 +0200

    codestyle: print_number

--- cJSON.c
+++ cJSON.c
@@ -209,32 +209,43 @@ static int update(printbuffer *p) {
 }
 
 /* Render the number nicely from the given item into a string. */
-static char *print_number(cJSON *item,printbuffer *p)
-{
-	char *str=0;
-	double d=item->valuedouble;
-	if (d==0)
-	{
-		if (p)	str=ensure(p,2);
-		else	str=(char*)cJSON_malloc(2);	/* special case for 0. */
-		if (str) strcpy(str,"0");
-	}
-	else if (fabs(((double)item->valueint)-d)<=DBL_EPSILON && d<=INT_MAX && d>=INT_MIN)
-	{
-		if (p)	str=ensure(p,21);
-		else	str=(char*)cJSON_malloc(21);	/* 2^64+1 can be represented in 21 chars. */
-		if (str)	sprintf(str,"%d",item->valueint);
-	}
-	else
-	{
-		if (p)	str=ensure(p,64);
-		else	str=(char*)cJSON_malloc(64);	/* This is a nice tradeoff. */
-		if (str)
-		{
-			if (fpclassify(d) != FP_ZERO && !isnormal(d))				sprintf(str,"null");
-			else if (fabs(floor(d)-d)<=DBL_EPSILON && fabs(d)<1.0e60)	sprintf(str,"%.0f",d);
-			else if (fabs(d)<1.0e-6 || fabs(d)>1.0e9)					sprintf(str,"%e",d);
-			else														sprintf(str,"%f",d);
+static char *print_number(cJSON *item,printbuffer *p) {
+	char *str = 0;
+	double d = item->valuedouble;
+	if (d == 0) {
+		if (p) {
+			str = ensure(p, 2);
+		} else {
+			str = (char*)cJSON_malloc(2); /* special case for 0. */
+		}
+		if (str) {
+			strcpy(str, "0");
+		}
+	} else if ((fabs(((double)item->valueint) - d) <= DBL_EPSILON) && (d <= INT_MAX) && (d >= INT_MIN)) {
+		if (p) {
+			str = ensure(p, 21);
+		} else {
+			str = (char*)cJSON_malloc(21); /* 2^64+1 can be represented in 21 chars. */
+		}
+		if (str) {
+			sprintf(str, "%d", item->valueint);
+		}
+	} else {
+		if (p) {
+			str = ensure(p, 64);
+		} else {
+			str = (char*)cJSON_malloc(64); /* This is a nice tradeoff. */
+		}
+		if (str) {
+			if ((fpclassify(d) != FP_ZERO) && (!isnormal(d))) {
+				sprintf(str,"null");
+			} else if ((fabs(floor(d) - d) <= DBL_EPSILON) && (fabs(d) < 1.0e60)) {
+				sprintf(str, "%.0f", d);
+			} else if ((fabs(d) < 1.0e-6) || (fabs(d) > 1.0e9)) {
+				sprintf(str, "%e", d);
+			} else {
+				sprintf(str,"%f",d);
+			}
 		}
 	}
 	return str;
