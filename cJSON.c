ef3939113c6bf24b6367c076fc8b91aac057c520
Max Bruckner <max@maxbruckner.de>
2015-10-17 03:57:13 +0200

    codestyle: cJSON_ParseWithOpts

--- cJSON.c
+++ cJSON.c
@@ -534,19 +534,32 @@ static const char *skip(const char *in) {
 }
 
 /* Parse an object - create a new root, and populate. */
-cJSON *cJSON_ParseWithOpts(const char *value,const char **return_parse_end,int require_null_terminated)
-{
-	const char *end=0;
-	cJSON *c=cJSON_New_Item();
-	ep=0;
-	if (!c) return 0;       /* memory fail */
+cJSON *cJSON_ParseWithOpts(const char *value, const char **return_parse_end, int require_null_terminated) {
+	const char *end = 0;
+	cJSON *c = cJSON_New_Item();
+	ep = 0;
+	if (!c) { /* memory fail */
+		return 0;
+	}
 
-	end=parse_value(c,skip(value));
-	if (!end)	{cJSON_Delete(c);return 0;}	/* parse failure. ep is set. */
+	end = parse_value(c, skip(value));
+	if (!end) { /* parse failure. ep is set. */
+		cJSON_Delete(c);
+		return 0;
+	}
 
 	/* if we require null-terminated JSON without appended garbage, skip and then check for a null terminator */
-	if (require_null_terminated) {end=skip(end);if (*end) {cJSON_Delete(c);ep=end;return 0;}}
-	if (return_parse_end) *return_parse_end=end;
+	if (require_null_terminated) {
+		end = skip(end);
+		if (*end) {
+			cJSON_Delete(c);
+			ep = end;
+			return 0;
+		}
+	}
+	if (return_parse_end) {
+		*return_parse_end = end;
+	}
 	return c;
 }
 /* Default options for cJSON_Parse */
