4fae236427e78fd25528b4b0972492a65711594d
Max Bruckner <max@maxbruckner.de>
2015-10-17 11:40:02 +0200

    codestyle: cJSON_DetachItemFromArray

--- cJSON.c
+++ cJSON.c
@@ -1227,8 +1227,28 @@ void cJSON_AddItemReferenceToObject(cJSON *object, const char *string, cJSON *it
 	cJSON_AddItemToObject(object, string, create_reference(item));
 }
 
-cJSON *cJSON_DetachItemFromArray(cJSON *array,int which)			{cJSON *c=array->child;while (c && which>0) c=c->next,which--;if (!c) return 0;
-	if (c->prev) c->prev->next=c->next;if (c->next) c->next->prev=c->prev;if (c==array->child) array->child=c->next;c->prev=c->next=0;return c;}
+cJSON *cJSON_DetachItemFromArray(cJSON *array, int which) {
+	cJSON *c = array->child;
+	while (c && (which>0)) {
+		c = c->next;
+		which--;
+	}
+	if (!c) {
+		return 0;
+	}
+	if (c->prev) {
+		c->prev->next = c->next;
+	}
+	if (c->next) {
+		c->next->prev = c->prev;
+	}
+	if (c == array->child) {
+		array->child = c->next;
+	}
+	c->next = 0;
+	c->prev = 0;
+	return c;
+}
 void   cJSON_DeleteItemFromArray(cJSON *array,int which)			{cJSON_Delete(cJSON_DetachItemFromArray(array,which));}
 cJSON *cJSON_DetachItemFromObject(cJSON *object,const char *string) {int i=0;cJSON *c=object->child;while (c && cJSON_strcasecmp(c->string,string)) i++,c=c->next;if (c) return cJSON_DetachItemFromArray(object,i);return 0;}
 void   cJSON_DeleteItemFromObject(cJSON *object,const char *string) {cJSON_Delete(cJSON_DetachItemFromObject(object,string));}
