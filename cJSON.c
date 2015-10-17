86603ac020cd62a402772e264a300ffad62f9046
Max Bruckner <max@maxbruckner.de>
2015-10-17 03:44:29 +0200

    codestyle: cJSON_Delete

--- cJSON.c
+++ cJSON.c
@@ -87,17 +87,21 @@ static cJSON *cJSON_New_Item(void) {
 }
 
 /* Delete a cJSON structure. */
-void cJSON_Delete(cJSON *c)
-{
+void cJSON_Delete(cJSON *c) {
 	cJSON *next;
-	while (c)
-	{
-		next=c->next;
-		if (!(c->type&cJSON_IsReference) && c->child) cJSON_Delete(c->child);
-		if (!(c->type&cJSON_IsReference) && c->valuestring) cJSON_free(c->valuestring);
-		if (!(c->type&cJSON_StringIsConst) && c->string) cJSON_free(c->string);
+	while (c) {
+		next = c->next;
+		if (!(c->type & cJSON_IsReference) && c->child) {
+			cJSON_Delete(c->child);
+		}
+		if (!(c->type & cJSON_IsReference) && c->valuestring) {
+			cJSON_free(c->valuestring);
+		}
+		if (!(c->type & cJSON_StringIsConst) && c->string) {
+			cJSON_free(c->string);
+		}
 		cJSON_free(c);
-		c=next;
+		c = next;
 	}
 }
 
