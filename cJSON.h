f20b7f88a582c4a0d9c0068eef352dd0059602ec
Max Bruckner <max@maxbruckner.de>
2015-10-17 15:42:28 +0200

    Use enum instead of defines for data types

--- cJSON.h
+++ cJSON.h
@@ -29,13 +29,15 @@ extern "C"
 #endif
 
 /* cJSON Types: */
-#define cJSON_False 0
-#define cJSON_True 1
-#define cJSON_NULL 2
-#define cJSON_Number 3
-#define cJSON_String 4
-#define cJSON_Array 5
-#define cJSON_Object 6
+typedef enum cJSON_Type {
+	cJSON_False = 0,
+	cJSON_True = 1,
+	cJSON_NULL = 2,
+	cJSON_Number = 3,
+	cJSON_String = 4,
+	cJSON_Array = 5,
+	cJSON_Object = 6
+} cJSON_Type;
 
 #define cJSON_IsReference 256
 #define cJSON_StringIsConst 512
@@ -45,7 +47,7 @@ typedef struct cJSON {
 	struct cJSON *next,*prev;	/* next/prev allow you to walk array/object chains. Alternatively, use GetArraySize/GetArrayItem/GetObjectItem */
 	struct cJSON *child;		/* An array or object item will have a child pointer pointing to a chain of the items in the array/object. */
 
-	int type;					/* The type of the item, as above. */
+	cJSON_Type type;					/* The type of the item, as above. */
 
 	char *valuestring;			/* The item's string, if type==cJSON_String */
 	int valueint;				/* The item's number, if type==cJSON_Number */
