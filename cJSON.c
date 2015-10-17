7f9204fc99174b2587de9d1b3668d7982d035260
Max Bruckner <max@maxbruckner.de>
2015-10-17 11:17:14 +0200

    codestyle: suffix_object

--- cJSON.c
+++ cJSON.c
@@ -1167,7 +1167,10 @@ cJSON *cJSON_GetObjectItem(cJSON *object, const char *string) {
 }
 
 /* Utility for array list handling. */
-static void suffix_object(cJSON *prev,cJSON *item) {prev->next=item;item->prev=prev;}
+static void suffix_object(cJSON *prev, cJSON *item) {
+	prev->next = item;
+	item->prev = prev;
+}
 /* Utility for handling references. */
 static cJSON *create_reference(cJSON *item) {cJSON *ref=cJSON_New_Item();if (!ref) return 0;memcpy(ref,item,sizeof(cJSON));ref->string=0;ref->type|=cJSON_IsReference;ref->next=ref->prev=0;return ref;}
 
