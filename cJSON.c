ec2e603a62117a3b945cdbd674f6850462f150b9
Max Bruckner <max@maxbruckner.de>
2015-10-17 11:10:00 +0200

    codestyle: cJSON_PrintUnformatted

--- cJSON.c
+++ cJSON.c
@@ -571,7 +571,9 @@ cJSON *cJSON_Parse(const char *value) {
 char *cJSON_Print(cJSON *item) {
 	return print_value(item, 0, 1, 0);
 }
-char *cJSON_PrintUnformatted(cJSON *item)	{return print_value(item,0,0,0);}
+char *cJSON_PrintUnformatted(cJSON *item) {
+	return print_value(item, 0, 0, 0);
+}
 
 char *cJSON_PrintBuffered(cJSON *item,int prebuffer,int fmt)
 {
