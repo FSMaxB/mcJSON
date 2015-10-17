eb8207b95d485ce76abe82b0f3dee2771aabef7c
Max Bruckner <max@maxbruckner.de>
2015-10-17 03:48:36 +0200

    codestyle: parse_hex4

--- cJSON.c
+++ cJSON.c
@@ -251,16 +251,51 @@ static char *print_number(cJSON *item,printbuffer *p) {
 	return str;
 }
 
-static unsigned parse_hex4(const char *str)
-{
-	unsigned h=0;
-	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
-	h=h<<4;str++;
-	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
-	h=h<<4;str++;
-	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
-	h=h<<4;str++;
-	if (*str>='0' && *str<='9') h+=(*str)-'0'; else if (*str>='A' && *str<='F') h+=10+(*str)-'A'; else if (*str>='a' && *str<='f') h+=10+(*str)-'a'; else return 0;
+//TODO does this work with both big and little endian?
+static unsigned parse_hex4(const char *str) {
+	unsigned h = 0;
+	if ((*str >= '0') && (*str <= '9')) {
+		h += (*str) - '0';
+	} else if ((*str >= 'A') && (*str<='F')) {
+		h += 10 + (*str) - 'A';
+	} else if ((*str >= 'a') && (*str <= 'f')) {
+		h+= 10 + (*str) - 'a';
+	} else {
+		return 0;
+	}
+	h = h << 4;
+	str++;
+	if ((*str >= '0') && (*str <= '9')) {
+		h += (*str) - '0';
+	} else if ((*str >= 'A') && (*str<='F')) {
+		h += 10 + (*str) - 'A';
+	} else if ((*str >= 'a') && (*str <= 'f')) {
+		h+= 10 + (*str) - 'a';
+	} else {
+		return 0;
+	}
+	h = h << 4;
+	str++;
+	if ((*str >= '0') && (*str <= '9')) {
+		h += (*str) - '0';
+	} else if ((*str >= 'A') && (*str<='F')) {
+		h += 10 + (*str) - 'A';
+	} else if ((*str >= 'a') && (*str <= 'f')) {
+		h+= 10 + (*str) - 'a';
+	} else {
+		return 0;
+	}
+	h = h << 4;
+	str++;
+	if ((*str >= '0') && (*str <= '9')) {
+		h += (*str) - '0';
+	} else if ((*str >= 'A') && (*str<='F')) {
+		h += 10 + (*str) - 'A';
+	} else if ((*str >= 'a') && (*str <= 'f')) {
+		h+= 10 + (*str) - 'a';
+	} else {
+		return 0;
+	}
 	return h;
 }
 
