51396068eeebb93772fa3db4abd592cd717c3fae
Max Bruckner <max@maxbruckner.de>
2015-10-25 12:47:07 +0100

    test-objects: Put tests into an array that is looped over

--- test/test-objects.c
+++ test/test-objects.c
@@ -202,42 +202,33 @@ int main (int argc, char **argv) {
 	}
 
 	/* a bunch of json: */
-	char text1[] = "{\n\"name\": \"Jack (\\\"Bee\\\") Nimble\", \n\"format\": {\"type\":       \"rect\", \n\"width\":      1920, \n\"height\":     1080, \n\"interlace\":  false,\"frame rate\": 24\n}\n}";
-	char text2[] = "[\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]";
-	char text3[] = "[\n    [0, -1, 0],\n    [1, 0, 0],\n    [0, 0, 1]\n	]\n";
-	char text4[] = "{\n		\"Image\": {\n			\"Width\":  800,\n			\"Height\": 600,\n			\"Title\":  \"View from 15th Floor\",\n			\"Thumbnail\": {\n				\"Url\":    \"http:/*www.example.com/image/481989943\",\n				\"Height\": 125,\n				\"Width\":  \"100\"\n			},\n			\"IDs\": [116, 943, 234, 38793]\n		}\n	}";
-	char text5[] = "[\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.7668,\n	 \"Longitude\": -122.3959,\n	 \"Address\":   \"\",\n	 \"City\":      \"SAN FRANCISCO\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94107\",\n	 \"Country\":   \"US\"\n	 },\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.371991,\n	 \"Longitude\": -122.026020,\n	 \"Address\":   \"\",\n	 \"City\":      \"SUNNYVALE\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94085\",\n	 \"Country\":   \"US\"\n	 }\n	 ]";
+	char *json[] = {
+		"{\n\"name\": \"Jack (\\\"Bee\\\") Nimble\", \n\"format\": {\"type\":       \"rect\", \n\"width\":      1920, \n\"height\":     1080, \n\"interlace\":  false,\"frame rate\": 24\n}\n}",
+
+		"[\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]",
+		"[\n    [0, -1, 0],\n    [1, 0, 0],\n    [0, 0, 1]\n	]\n",
+		"{\n		\"Image\": {\n			\"Width\":  800,\n			\"Height\": 600,\n			\"Title\":  \"View from 15th Floor\",\n			\"Thumbnail\": {\n				\"Url\":    \"http:/*www.example.com/image/481989943\",\n				\"Height\": 125,\n				\"Width\":  \"100\"\n			},\n			\"IDs\": [116, 943, 234, 38793]\n		}\n	}",
+	"[\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.7668,\n	 \"Longitude\": -122.3959,\n	 \"Address\":   \"\",\n	 \"City\":      \"SAN FRANCISCO\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94107\",\n	 \"Country\":   \"US\"\n	 },\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.371991,\n	 \"Longitude\": -122.026020,\n	 \"Address\":   \"\",\n	 \"City\":      \"SUNNYVALE\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94085\",\n	 \"Country\":   \"US\"\n	 }\n	 ]"
+	};
 
 	/* Process each json textblock by parsing, then rebuilding: */
-	int status;
-	status = doit(text1, output_file);
-	if (status == 0) {
-		fprintf(stderr, "Failed on text1!\n");
-		return EXIT_FAILURE;
-	}
-	status = doit(text2, output_file);
-	if (status == 0) {
-		fprintf(stderr, "Failed on text2!\n");
-		return EXIT_FAILURE;
-	}
-	status = doit(text3, output_file);
-	if (status == 0) {
-		fprintf(stderr, "Failed on text3!\n");
-		return EXIT_FAILURE;
-	}
-	status = doit(text4, output_file);
-	if (status == 0) {
-		fprintf(stderr, "Failed on text4!\n");
-		return EXIT_FAILURE;
-	}
-	status = doit(text5, output_file);
-	if (status == 0) {
-		fprintf(stderr, "Failed on text5!\n");
-		return EXIT_FAILURE;
+	for (size_t i = 0; i < (sizeof(json) / sizeof(char*)); i++) {
+		int status = doit(json[i], output_file);
+		if (status == 0) {
+			fprintf(stderr, "ERROR: Failed on text %zi!\n", i);
+			if (output_file != NULL) {
+				fclose(output_file);
+			}
+			return EXIT_FAILURE;
+		}
 	}
 
 	/* Now some samplecode for building objects concisely: */
 	create_objects(output_file);
+
+	if (output_file != NULL) {
+		fclose(output_file);
+	}
 
 	return EXIT_SUCCESS;
 }
