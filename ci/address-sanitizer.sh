d54921d9f700158d8b10e78b8ba24c910afe1570
Max Bruckner <max@maxbruckner.de>
2015-12-17 18:15:30 +0100

    ci: check if address sanitizer is available before using it

--- ci/address-sanitizer.sh
+++ ci/address-sanitizer.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-if [ $(uname) == "Darwin" ]; then
-    #Skip test on Mac OS X
-    #TODO Make this work!
-    exit 0
-fi
 [ ! -e address-sanitizer ] && mkdir address-sanitizer
 cd address-sanitizer
+#check if address sanitizer is available
+echo "int main(void) {return 0;}" > test.c
+if ! clang -fsanitize=address test.c -o /dev/null > /dev/null; then
+    echo AddressSanitizer not available. Skipping ...
+    rm test.c
+    exit 0
+fi
+rm test.c
+
 export CC=clang
 cmake .. -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS='-fsanitize=address -O1 -fno-omit-frame-pointer -fno-common -fno-optimize-sibling-calls -g' -DDISABLE_MEMORYCHECK_COMMAND="TRUE"
 make clean
